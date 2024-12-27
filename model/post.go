c8bccb2180c2d6474edde0b93baba0fa3519dbb2
nikitakuharcuk <nikitakukharchuk@mail.ru>
2024-12-27 17:08:36 +0300

    try to fix error with creating post and insert in db

--- model/post.go
+++ model/post.go
@@ -1,21 +1,19 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Post struct {
 	gorm.Model
-	ID    uuid.UUID `gorm:"type:uuid;"`
-	Title string    `json:"title"`
-	Body  string    `json:"body"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
 }
 type Posts struct {
 	Posts []Post `json:"posts"`
 }
 
-func (post *Post) BeforeCreate(tx *gorm.DB) (err error) {
-	post.ID = uuid.New()
-	return
-}
+//func (post *Post) BeforeCreate(tx *gorm.DB) (err error) {
+//	post.ID = uuid.New()
+//	return
+//}
